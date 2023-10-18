dc32fe6a705943e6b0e8c798f9cf3708156139d6
DestinyMatuse <lillymatuse@gmail.com>
2023-10-18 05:36:18 -0700

    update pop_file

---
+++ pop_file.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * F_Pop - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void F_Pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+	fclose(bus.file);
+	free(bus.content);
+	freeStack(*head);
+	exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
+
